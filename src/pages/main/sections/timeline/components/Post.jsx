19c7e8fc27742369e480db3a5b6735a59ecd10d7
hayook <hay.eddine8@gmail.com>
2022-12-01 16:12:26 +0100

    Posts Section V3.4 - Comments V0.1 - em unit is awesome

--- src/pages/main/sections/timeline/components/Post.jsx
+++ src/pages/main/sections/timeline/components/Post.jsx
@@ -5,6 +5,9 @@ import SvgIcon from '../../../../../assets/icons/SvgIcon'
 import { icons } from '../../../../../assets/icons/icons'
 import { useGlobalState } from '../../../../../app/GlobalStateProvider';
 
+// refactore the main style.css file and this file
+// The comment structure is the same as the post so make a post component and call it recursively
+
 export default function Post({ postOwnerId, postId, title, body, date }) {
 
     const [comment, setComment] = useState('');
@@ -26,24 +29,56 @@ export default function Post({ postOwnerId, postId, title, body, date }) {
 
     return (
         <div className="post">
-            <UserProfileShowcase userId={postOwnerId} date={date}/>
-            <p className="post-content">{`${title}\n${body}`}</p>
+            <UserProfileShowcase userId={postOwnerId} date={date} />
+            <p className="post-content after-profile-content">{`${title}\n${body}`}</p>
             <div className="functionalities">
-                <button onClick={likePost}><SvgIcon path={icons.like} fill={liked && 'white'} /></button>
-                <button onClick={() => setShowComments(prev => !prev)}><SvgIcon path={icons.comment} /></button>
+                <div className="like-post">
+                    <button onClick={likePost}><SvgIcon path={icons.like} fill={liked && 'white'} /></button>
+                    <span>40k</span>
+                </div>
+                <div className="comment-on-post">
+                    <button onClick={() => setShowComments(prev => !prev)}><SvgIcon path={icons.comment} /></button>
+                    <span>7.2k</span>
+                </div>
             </div>
             {
                 showComments &&
-                <form className="comment">
-                    <textarea
-                        className="main-textarea"
-                        rows={1}
-                        placeholder='Write A Comment'
-                        value={comment}
-                        onChange={({ target }) => setComment(target.value)}
-                    ></textarea>
-                    <button className="submit-comment" onClick={commentOnPost}><BiSend /></button>
-                </form>
+                <>
+                    <div className="comments">
+                        <h3>Comments</h3>
+                        <div className="comment">
+                            <UserProfileShowcase userId={postOwnerId} date={date} />
+                            <p className="comment-body after-profile-content">This is an awesome comment</p>
+                            <button><SvgIcon path={icons.like} fill={liked && 'white'} /></button>
+                        </div><div className="comment">
+                            <UserProfileShowcase userId={postOwnerId} date={date} />
+                            <p className="comment-body after-profile-content">This is an awesome comment</p>
+                            <button><SvgIcon path={icons.like} fill={liked && 'white'} /></button>
+                        </div><div className="comment">
+                            <UserProfileShowcase userId={postOwnerId} date={date} />
+                            <p className="comment-body after-profile-content">This is an awesome comment</p>
+                            <button><SvgIcon path={icons.like} fill={liked && 'white'} /></button>
+                        </div><div className="comment">
+                            <UserProfileShowcase userId={postOwnerId} date={date} />
+                            <p className="comment-body after-profile-content">This is an awesome comment</p>
+                            <button><SvgIcon path={icons.like} fill={liked && 'white'} /></button>
+                        </div><div className="comment">
+                            <UserProfileShowcase userId={postOwnerId} date={date} />
+                            <p className="comment-body after-profile-content">This is an awesome comment</p>
+                            <button><SvgIcon path={icons.like} fill={liked && 'white'} /></button>
+                        </div>
+                    </div>
+                    <form className="comment">
+                        <textarea
+                            className="main-textarea"
+                            rows={1}
+                            placeholder='Write A Comment'
+                            value={comment}
+                            onChange={({ target }) => setComment(target.value)}
+                        ></textarea>
+                        <button className="submit-comment" onClick={commentOnPost}><BiSend /></button>
+                    </form>
+                </>
             }
         </div>
     )
