e24c7bf4c0fa8bd9a2f8785999b714298ef69919
hayook <hay.eddine8@gmail.com>
2022-12-02 14:52:55 +0100

    Posts Section V3.6 - Share Other Types

--- src/pages/main/sections/timeline/components/SharePostSection.jsx
+++ src/pages/main/sections/timeline/components/SharePostSection.jsx
@@ -1,31 +1,54 @@
 import { useState } from 'react'
 import UserProfileShowcase from '../../../../../components/user-profile-showcase/UserProfileShowcase';
 import { useGlobalState } from '../../../../../app/GlobalStateProvider';
-import { headers } from '../../../../../app/api'
 
 export default function SharePostSection() {
 
     const [postBody, setPostBody] = useState('');
+    const [showFunctionalities, setShowFunctionalities] = useState(false);
+    const [rows, setRows] = useState(3)
+
     const { state } = useGlobalState();
     const { userId } = state.user;
 
     const sharePost = () => {
-        console.log({ user_id: userId, post_description: postBody }); 
+        console.log({ user_id: userId, post_description: postBody });
         setPostBody('');
     }
 
+    const handleSharePostTextarea = ({ target }) => {
+        setPostBody(target.value)
+    }
+
     return (
+        <>
         <section className="share-post">
-            <UserProfileShowcase userId={userId} />
-            <textarea 
-            value={postBody} 
-            onChange={({ target }) => setPostBody(target.value)} 
-            className="main-textarea" 
-            placeholder="Write Something"
+            <div className="profile-img"></div>
+            <textarea
+                rows={rows}
+                value={postBody}
+                onChange={handleSharePostTextarea}
+                onFocus={() => setShowFunctionalities(true)}
+                className="main-textarea"
+                placeholder="Write Something"
             ></textarea>
+            {showFunctionalities &&
+                <div className="functionalities">
+                    <button onClick={sharePost} className="post main-button">Post</button>
+                </div>
+            }
+        </section>
+        <section className="other-posts-types">
+            <div className="or">
+                <hr />
+                <span>OR</span>
+                <hr />
+            </div>
             <div className="functionalities">
-                <button onClick={sharePost} className="post main-button">Post</button>
+                <button className="secondary-button">Ask A Question</button>
+                <button className="secondary-button">Share Your Work</button>
             </div>
         </section>
+        </>
     )
 }
