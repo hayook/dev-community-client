a1b1a1a98859966be91eafade1d12dc242db11a7
hayook <hay.eddine8@gmail.com>
2022-12-22 15:16:45 +0100

    Updated: TeamsTab : Create Team | Read Team Members | Add Member To Team Setup - Dashboard V5.4

--- src/pages/dashboard-page/components/TeamsList.jsx
+++ src/pages/dashboard-page/components/TeamsList.jsx
@@ -1,9 +1,51 @@
+import { useState } from 'react';
 import { projects } from '../../../trash/test-data'
+import ProjectMember from './ProjectMember'
+import { BiArrowBack } from 'react-icons/bi'
+import MembersList from './MembersList'
 
 export default function TeamsList() {
+
+    const [teamMembers, setTeamMembers] = useState([]);
+    const [addMemberToTeam, setAddMemberToTeam] = useState(false);
+
+    const handleClick = () => { }
+
+    const showTeamMembers = (id) => {
+        setTeamMembers(projects[0].projectTeams.filter(team => team.id === id)[0].teamMembers);
+    }
+
     return (
-        <div className="teams-list">
-            { projects[0].projectTeams.map(team => <div className='project-team' key={team.id}>{ team.name }</div>)}
+        <div className={`teams-list-container ${(teamMembers.length !== 0 && !addMemberToTeam) ? 'active' : ''}`}>
+            {!addMemberToTeam &&
+                <div className="teams-list">
+                    {projects[0].projectTeams.map(team => <button onClick={() => showTeamMembers(team.id)}><div className='project-team' key={team.id}>{team.name}</div></button>)}
+                </div>
+            }
+            {teamMembers.length !== 0 &&
+                <div className={`team-members-list ${addMemberToTeam ? 'active' : ''}`}>
+                    <div className="heading">
+                        <h2>{ addMemberToTeam ? 'Add Member' : 'Team Members'}</h2>
+
+                        {addMemberToTeam ?
+                            <button onClick={() => setAddMemberToTeam(false)} className='back-button'><BiArrowBack /></button>
+                            :
+                            <button onClick={() => setAddMemberToTeam(true)} className='main-button'>Add Member</button>
+                        }
+
+                    </div>
+                    {addMemberToTeam ?
+                        <div className="search-members">
+                            <input type="text" className="main-input" placeholder='search' />
+                            <MembersList membersList={projects[0].projectMembers} />
+                        </div>
+                        :
+                        <div className="members-list">
+                            {teamMembers.map(member => <ProjectMember memberId={member.userId} />)}
+                        </div>
+                    }
+                </div>
+            }
         </div>
     )
 }
--- src/pages/dashboard-page/components/TeamsTab.jsx
+++ src/pages/dashboard-page/components/TeamsTab.jsx
@@ -1,12 +1,32 @@
+import { useState } from 'react'
 import TeamsList from './TeamsList'
+import { BiArrowBack } from 'react-icons/bi'
+import ProjectMember from './ProjectMember'
+
 
 export default function TeamsTab() {
+
+    const [createTeamForm, setCreateTeamForm] = useState(false);
+
     return (
         <>
-        <div className="heading">
-            <h1>Project Teams</h1>
-        </div>
-        <TeamsList />
+            <div className="heading">
+                <h1>Project Teams</h1>
+                {createTeamForm ?
+                    <button onClick={() => setCreateTeamForm(false)} className='back-button'><BiArrowBack /></button>
+                    :
+                    <button onClick={() => setCreateTeamForm(true)} className='main-button'>Create Team</button>
+                }
+            </div>
+            {createTeamForm ?
+                <form className="create-team-form">
+                    <label>Team Name</label>
+                    <input type="text" className="main-input" />
+                    <button className='main-button'>Submit</button>
+                </form>
+                :
+                <TeamsList />
+            }
         </>
     )
 }
