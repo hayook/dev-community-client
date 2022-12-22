a21acf2d8df16b14d63dc1ad6e6c131015b100d3
hayook <hay.eddine8@gmail.com>
2022-12-22 18:59:34 +0100

    Added: Invites Section

--- src/pages/components/working-rooms/WorkingRooms.jsx
+++ src/pages/components/working-rooms/WorkingRooms.jsx
@@ -5,12 +5,24 @@ import './style.css'
 export default function WorkingRooms() {
     return (
         <section className="working-rooms">
-            <div className="working-rooms-container">
+            <div className="current-projects">
                 <h2>Your Current Projects</h2>
-                <div className="working-rooms-projects">
+                <div className="projects-list">
                     <ProjectShowcase />
                 </div>
                 <Link to="/projects/new" className="main-button">Start A New Project</Link>
+            </div>
+            <div className="user-invites">
+                <h2>Invites</h2>
+                <div className="invites-list">
+                    <div className="invite">
+                        <h4>Project Name</h4>
+                        <div className="functionalities">
+                            <button className="main-button">Accept</button>
+                            <button className="main-button reject">Reject</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
