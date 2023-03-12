b7cc1758e29bd94e5374e1293a55dceec6a884ea
hayook <hay.eddine8@gmail.com>
2023-03-12 19:15:35 +0100

    Drop Icon transition

--- src/trash/TestComp.jsx
+++ src/trash/TestComp.jsx
@@ -1,8 +1,28 @@
-import ProgressBar from "../pages/components/progress-bar/ProgressBar";
+import { useState } from 'react';
+import Main from '../pages/components/main/Main'
+
 
 export default function TestComp() {
 
+  const [dark, setDark] = useState(true);
+
+  const handleTheme = e => setDark(prev => !prev);
+
   return (
-    <ProgressBar progress={10} />
+    <Main>
+      <Show when={dark}>
+        <h1>Dark</h1>
+      </Show>
+      <Show when={!dark}>
+      <h1>Light</h1>
+      </Show>
+
+      <button className="main-button" onClick={handleTheme}>Switch</button>
+    </Main>
   )
+}
+
+
+function Show({ children, when }) {
+  if (when) return children;
 }
