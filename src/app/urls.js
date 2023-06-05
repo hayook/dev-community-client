1aa590c524664ca8e5e27fc30de7dbf3a70932a8
Hayeddine <hay.eddine8@gmail.com>
2023-06-05 21:11:34 +0100

    05

--- src/app/urls.js
+++ src/app/urls.js
@@ -1,5 +1,5 @@
 export const URLS = {
-    API: 'http://localhost:3000',
-    CHAT: 'ws://localhost:5000/ws',
-    STATIC: 'http://localhost:4000',
+	API: 'http://localhost:3000',
+    CHAT: 'http://localhost:5000',
+    STATIC: 'http://file.devcommunity.tech',
 }
